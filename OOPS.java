9e9724550d8581392333c57e581fba4e737cb81f
PRISHA SHAH <136842969+PrishaShah30@users.noreply.github.com>
2024-01-15 00:33:09 -0500

    OOPS.java

--- OOPS.java
+++ OOPS.java
@@ -1,47 +1,47 @@
-// is the pen class with no static void nothing 
-class pen{ // data
-    String color; 
+class pen {
+    String color;
     String type;
 
-    public void print(){ //members
+    public void print() {
         System.out.println(this.color);
     }
 
-    public void write(){ //members
+    public void write() {
         System.out.println(this.type);
     }
 }
-class Student{
+
+class Student {
     String name;
     int age;
 
-    public void printName(){
+    public void printName() {
         System.out.println(this.name);
         System.out.println(this.age);
     }
-    Student(String name, int age){
+
+    // Constructor for the Student class
+    Student(String name, int age) {
         this.name = name;
         this.age = age;
     }
 }
+
 public class OOPS {
-    public static void main(String args[]){
-    pen pen1 = new pen();
-    // made an object here 
-    pen1.color = "blue";
-    pen1.type = "gel";
+    public static void main(String args[]) {
+        pen pen1 = new pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
 
-    pen pen2 = new pen();
-    pen2.color ="black";
-    pen2.type = "ball";
-    
-    pen1.print();
-    pen2.write();
+        pen pen2 = new pen();
+        pen2.color = "black";
+        pen2.type = "ball";
 
-    Student s = new Student();
-    s.name = "Prisha";
-    s.age = 22;
+        pen1.print();
+        pen1.write(); // Corrected method name
 
-    s.printName();
+        Student s = new Student("Prisha", 22); // Provide constructor parameters
+
+        s.printName();
     }
 }
