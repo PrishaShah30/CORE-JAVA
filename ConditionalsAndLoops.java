96577e1ed40e3fb15dbff74107a63c2ea824f82b
PRISHA SHAH <136842969+PrishaShah30@users.noreply.github.com>
2024-01-15 00:22:14 -0500

    ConditionalsAndLoops.java

--- ConditionalsAndLoops.java
+++ ConditionalsAndLoops.java
@@ -1,56 +1,47 @@
-// Prisha Shah
-// 11th Feb 2023 
-// If Else , Else if -- These are a kind of keywords 
-
 public class ConditionalsAndLoops {
-    public static void main(String args[]){
-        // if(Math.random()<0.5){
-        //     System.out.println("heads");
-        // }
-        // else{
+    public static void main(String args[]) {
+        // Example of a simple coin toss simulation
+        // if (Math.random() < 0.5) {
+        //     System.out.println("Heads");
+        // } else {
         //     System.out.println("Tails");
         // }
 
-// TwoSort
-// int a = Integer.parseInt(args[0]);
-// int b = Integer.parseInt(args[1]);
-// if(b<a){
-//     int t=a;
-//     a=b;
-//     b=t;
-// }
-// System.out.println(a);
-// System.out.println(b);
+        // Two Sort: Sorting two numbers in ascending order
+        // int a = Integer.parseInt(args[0]);
+        // int b = Integer.parseInt(args[1]);
+        // if (b < a) {
+        //     int temp = a;
+        //     a = b;
+        //     b = temp;
+        // }
+        // System.out.println(a);
+        // System.out.println(b);
 
-// Three Sort 
-int a = Integer.parseInt(args[0]);
-int b = Integer.parseInt(args[1]);
-int c = Integer.parseInt(args[2]);
-if(a<b){
-    // b is bigger 
-    int temp = a;
-    a = b;
-    b = temp;
-}
-else{
-    if(b>c){
-        // if b is bigger 
-        int tem = b;
-        b = c;
-        c = tem;
-    }
-    else{
-        // c>a
-        int te = c;
-        c = a;
-        a = te;
-    }
-}
-System.out.println(a);
-System.out.println(b);
-System.out.println(c);
+        // Three Sort: Sorting three numbers in ascending order
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        if (a < b) {
+            // Swap a and b if b is greater
+            int temp = a;
+            a = b;
+            b = temp;
+        } else {
+            if (b > c) {
+                // Swap b and c if b is greater
+                int temp = b;
+                b = c;
+                c = temp;
+            } else {
+                // Swap c and a if c is greater
+                int temp = c;
+                c = a;
+                a = temp;
+            }
+        }
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
-
-
-
