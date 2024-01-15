f94e6a6ed0f8bbe8a2261a4aff054cb228766484
PRISHA SHAH <136842969+PrishaShah30@users.noreply.github.com>
2024-01-15 00:36:30 -0500

    SelectionSortFunction.java

--- SelectionSortFunction.java
+++ SelectionSortFunction.java
@@ -1,28 +1,34 @@
 public class SelectionSortFunction {
-    public static void printArray(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+    // Function to print the elements of the array
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-    public static void main(String args[]){
-        int arr[] = {7,8,3,2,1};
-        // selection sort 
-        // it'll assume the first index of the array to be the smallest and the check of the other elements if
-        // there is some other number smaller than assumed then it'll replace places with it 
-        // time complexity Big O(n^2) 
-        // even this is not an optimal sorting to use 
-        for(int i=0; i<arr.length-1;i++){
+
+    public static void main(String args[]) {
+        int arr[] = {7, 8, 3, 2, 1};
+        
+        // Selection Sort:
+        // It assumes the first index of the array to be the smallest and checks 
+        // the other elements to find a smaller number, replacing places when needed.
+        // Time complexity: O(n^2)
+        // Note: Selection Sort is not the most optimal sorting algorithm.
+        for (int i = 0; i < arr.length - 1; i++) {
             int smallest = i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[smallest]>arr[j]){
-                    smallest=j;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
                 }
             }
+            // Swapping elements to place the smallest in the correct position
             int temp = arr[smallest];
-            arr[smallest]=arr[i];
-            arr[i]=temp;
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
+
+        // Printing the sorted array
         printArray(arr);
     }
 }
