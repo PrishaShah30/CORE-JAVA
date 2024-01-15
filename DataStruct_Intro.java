983be89a36d2fe7b274ed188fa1eb38664e49199
PRISHA SHAH <136842969+PrishaShah30@users.noreply.github.com>
2024-01-15 00:42:02 -0500

    Add files via upload

---
+++ DataStruct_Intro.java
@@ -0,0 +1,28 @@
+public class DataStruct_Intro {
+    /*
+     * A data structure is like a tool that helps a computer organize and manage 
+     * information effectively. It not only stores data but also helps in doing things 
+     * like sorting, finding, and saving data. Different types of data structures are 
+     * used in almost every computer program to make it work smoothly.
+     * 
+     * There two primary types of data structures: 
+     * Linear Data Structures 
+     * Non-Linear Data Strcuctures 
+     * 
+     * Linear Data Structures 
+     * It refers to a type of data arrangement where elements are organized sequentially, 
+     * each linked to its preceding and succeeding elements. Examples of linear data structures 
+     * include arrays, stacks, queues, and linked lists. 
+     * 
+     * Static data structures have a fixed memory size, making it straightforward to access elements. 
+     * Arrays are a common example of a static data structure.
+     * 
+     * Dynamic data structures, on the other hand, don't have a fixed size and can be adjusted during 
+     * runtime. This flexibility can be advantageous for optimizing memory usage. Examples of dynamic 
+     * data structures include queues and stacks.
+     * 
+     * Non-linear data structures are characterized by elements not being placed in a sequential or 
+     * linear order. Traversing all elements in a single run is not possible in non-linear structures. 
+     * Trees and graphs are examples of non-linear data structures.
+     */
+}
