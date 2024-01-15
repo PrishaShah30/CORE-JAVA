2c3b17a47f18980ce7cc6708b61d3c9d768a5e24
PRISHA SHAH <136842969+PrishaShah30@users.noreply.github.com>
2024-01-15 00:37:40 -0500

    StringBuilderFunction.java

--- StringBuilderFunction.java
+++ StringBuilderFunction.java
@@ -1,33 +1,30 @@
 import java.util.*;
+
 public class StringBuilderFunction {
-    public static void main(String args[]){
-        // StringBuilder 
+    public static void main(String args[]) {
+        // StringBuilder initialization
         Scanner scr = new Scanner(System.in);
         StringBuilder prish = new StringBuilder(scr.nextLine());
         System.out.println(prish);
 
-        //charAt
+        // charAt: Accessing character at a specific index
         System.out.println(prish.charAt(4));
 
-        //set char at index 0
-        // it means to take a particular index and change it with something else 
-        // prish.setCharAt(index number , the change );
-        prish.setCharAt(3,'R');
+        // setCharAt: Changing character at a specific index
+        prish.setCharAt(3, 'R');
         System.out.println(prish);
 
-        //insertion 
-        // inserting soemthing for example name = tony and we want to add s to make it Stony 
-        prish.insert(4,'F');
+        // insertion: Inserting a character at a specific index
+        prish.insert(4, 'F');
         System.out.println(prish);
 
-        //deletion 
-        // deleting a part of the string 
-        // syntax string.delete(from this index, to this index);
-        prish.delete(4,7);
+        // deletion: Deleting a part of the string
+        // Syntax: string.delete(fromIndex, toIndex);
+        prish.delete(4, 7);
         System.out.println(prish);
 
-        //append which means add something at the end of the string 
-        prish.append("and not");
+        // append: Adding something at the end of the string
+        prish.append(" and not");
         System.out.println(prish);
     }
 }
