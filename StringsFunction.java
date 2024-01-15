1820a68290e6b4f801d9ec5acfb75f98b06c3029
PRISHA SHAH <136842969+PrishaShah30@users.noreply.github.com>
2024-01-15 00:38:05 -0500

    StringsFunction.java

--- StringsFunction.java
+++ StringsFunction.java
@@ -1,45 +1,41 @@
 public class StringsFunction {
-    public static void main(String args[]){
-    /*
-    Declaration of String :
-    String variable = “ input by the writter itself “;
-    String variable = sc.nextLine"();  — (by user via a scanner class)
-    String variable = new String();
-    String str = Integer.parseInt(str);
+    public static void main(String args[]) {
+        /*
+        Declaration of String :
+        String variable = "input by the writer itself";
+        String variable = sc.nextLine();  // (by the user via a Scanner class)
+        String variable = new String();
+        String str = Integer.parseInt(str);
         */
-    // String Tony = new String();
-    // Tony = "Hey there";
-    // System.out.println(Tony);
-    
-    // concatination 
-    String firstName= "tony";
-    String LastName = "Stark";
-    String Fullname = firstName + " " + LastName;
-    System.out.print(Fullname);
+        // Concatenation
+        String firstName = "Tony";
+        String lastName = "Stark";
+        String fullName = firstName + " " + lastName;
+        System.out.print(fullName);
 
-    // length to know 
-    System.out.println(Fullname.length());
-    //CharAt
-    for(int i=0;i<Fullname.length();i++){
-        System.out.print(Fullname.charAt(i));
+        // Length to know the length of the string
+        System.out.println(fullName.length());
+
+        // CharAt: Accessing characters at each index
+        for (int i = 0; i < fullName.length(); i++) {
+            System.out.print(fullName.charAt(i));
+        }
+
+        // CompareTo: Comparing two strings lexicographically
+        if (firstName.compareTo(lastName) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+        // Substring: Extracting a portion of the string
+        String sentence = "My name is Prish";
+        String name = sentence.substring(9, sentence.length());
+        System.out.println(name);
+
+        // ToString method to convert an integer to a string
+        int num = 123;
+        String strNum = Integer.toString(num);
+        System.out.println(strNum.length());
     }
-
-    //Compare to it is same as (firstName == LastName)
-    if(firstName.compareTo(LastName)== 0){
-        System.out.println("Strings are equal");
-    }    
-    else{
-        System.out.println("Strings are not equal");
-    }
-
-    //Substring :takes a beginning index and ending index 
-    String sentence = "My name is Prish";
-    String name = sentence.substring(9,sentence.length());
-    System.out.println(name);
-
-    //ToString method 
-    int num = 123;
-    String str = Integer.toString(num);
-    System.out.println(str.length());
-}
 }
